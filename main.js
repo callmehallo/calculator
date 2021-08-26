376a5f94e30392ba01b530232a5cf7b0a5a46398
Andi <vvorobjev@gmx.de>
2021-08-26 02:35:02 +0200

    bug fixes with calculator operations

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-"use script";
+let displayValue = 0; //am display angezeigt
+let firOperand = 0; // nummer 1
+let secOperand = 0; // nummer 2
+let firOperator = ""; // operator 1
+let result = 0;
+let clicked = false; // disable decimal-button
 
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
@@ -7,39 +12,122 @@ const divide = (a, b) => {
   if (b === 0) return "ERROR";
   else return a / b;
 };
-const operate = (op, a, b) => {};
+
+const operate = (a, op, b) => {
+  if (op === "/") {
+    return divide(a, b);
+  } else if (op === "*") {
+    return multiply(a, b);
+  } else if (op === "+") {
+    return add(a, b);
+  } else if (op === "-") {
+    return substract(a, b);
+  }
+};
+
+// DOM
 const calc = document.querySelector(".calc");
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".num");
-/* //numbers
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero");
-//operators
-const clear = document.querySelector("#clear");
-const vz = document.querySelector("#vz");
-const percent = document.querySelector("#percent");
-const times = document.querySelector("multiply");
-const minus = document.querySelector("minus");
-const plus = document.querySelector("plus");
-const equals = document.querySelector("#equals");
-const point = document.querySelector("point");
-const divideSign = document.querySelector("#divide"); */
-let dispVal = 0;
-display.textContent = `${dispVal}`;
-/* const calcFunc = e; */
+const deci = document.querySelector("#point");
+
+const outputDisplay = () => (display.textContent = `${displayValue}`); //zeigt die Nummer am display an
+const outputResult = () => (display.textContent = `${result}`); //zeigt Ergebnis an
+
+const operatorDecision = (e) => {
+  if (e.target.getAttribute("id") === "divide") return "/";
+  else if (e.target.getAttribute("id") === "multiply") return "*";
+  else return `${e.target.innerText}`;
+};
+
+const giveResult = (num1, opr, num2) => {
+  result = operate(Number(num1), opr, Number(num2));
+  if (result % 1 !== 0) {
+    result = parseFloat(result).toFixed(2);
+  }
+  outputResult();
+  firOperand = result;
+  secOperand = 0;
+  displayValue = 0;
+};
+const enableDecimals = () => {
+  displayValue = 0;
+  clicked = false;
+};
+const checkDecimal = () => {
+  if (displayValue % 1 !== 0) {
+    clicked = true;
+  } else {
+    clicked = false;
+  }
+};
 
 calc.addEventListener("click", (e) => {
-  if (e.target.getAttribute("id") === "clear") return (dispVal = "");
-  else if (e.target.hasAttribute("data-type")) {
-    dispVal = `${e.target.innerText}`;
-    return (display.textContent = `${dispVal}`);
+  if (e.target.hasAttribute("data-type")) {
+    if (displayValue == 0 && displayValue !== "0.") {
+      displayValue = `${e.target.innerText}`;
+    } else {
+      displayValue += `${e.target.innerText}`;
+    }
+    return outputDisplay();
+  } else if (e.target.hasAttribute("data-opr")) {
+    if (firOperand && firOperator !== "" && !secOperand && !displayValue) {
+      firOperator = operatorDecision(e);
+    }
+    if (!firOperator) {
+      if (!displayValue) firOperand = result;
+      else if (displayValue) firOperand = displayValue;
+      enableDecimals();
+      firOperator = operatorDecision(e);
+    } else if (firOperand && firOperator !== "") {
+      secOperand = displayValue;
+      enableDecimals();
+      giveResult(firOperand, firOperator, secOperand);
+      firOperator = operatorDecision(e);
+    }
+  } else if (e.target.getAttribute("id") === "vz") {
+    if (!displayValue) {
+      result *= -1;
+      outputResult();
+    } else {
+      displayValue *= -1;
+      outputDisplay();
+    }
+  } else if (e.target.getAttribute("id") === "percent") {
+    if (!displayValue) {
+      result *= 0.01;
+      outputResult();
+    } else {
+      displayValue *= 0.01;
+      outputDisplay();
+    }
+  }
+  if (e.target.getAttribute("id") === "point") {
+    checkDecimal();
+    if (!clicked) {
+      displayValue = `${displayValue}.`;
+      clicked = true;
+      outputDisplay();
+    } else return;
+  } else if (e.target.getAttribute("id") === "equals") {
+    if (!firOperator) {
+      enableDecimals();
+      return;
+    } else if (firOperator) {
+      if (!firOperand) firOperand = result;
+      secOperand = displayValue;
+      giveResult(firOperand, firOperator, secOperand);
+    }
+    firOperator = "";
+  } else if (e.target.getAttribute("id") === "clear") {
+    displayValue = 0;
+    secOperand = 0;
+    firOperand = 0;
+    firOperator = "";
+
+    result = 0;
+    clicked = false;
+
+    outputDisplay();
   }
 });
